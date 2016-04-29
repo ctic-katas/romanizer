c599238c7e9de7b04b8f35dfb6935bd6e51f0bda
oscar <oscar.tomico@fundacionctic.org>
2016-04-29 13:58:55 +0200

    First upload

--- romanizer.js
+++ romanizer.js
@@ -1,2 +1,47 @@
 function romanize (num) {
+    var resultado = "";
+/*        
+    if((num<4)){
+        for (var i=0;i<num;i++){
+            resultado+="I"
+        }
+    }
+    
+    if((num==4)){
+        resultado+="IV"
+    }
+    
+    if((num==5)){
+        resultado+="V"
+    }
+    
+    if((num>5 && num<9)){
+        resultado="V"
+        for (var i=6;i<=num;i++){
+            resultado+="I"
+        }
+    }*/
+    
+    // I, V, X, L, C ...
+    if(num % 1000 == 0){
+        resultado += "M";
+    }
+    else if(num % 500 == 0){
+        resultado += "D";
+    }
+        else if(num % 100 == 0){
+        resultado += "C";
+    }
+        else if(num % 50 == 0){
+        resultado += "L";
+    }
+        else if(num % 10 == 0){
+        resultado += "X";
+    }
+        else if(num % 5 == 0){
+        resultado += "V";
+    }
+    
+    return resultado;
+    
 }
