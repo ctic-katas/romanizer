0f9d58d63e5099b1f1cef1967669cf9e723c7f62
veronica <veronica.abad@fundacionctic.org>
2016-04-29 13:58:54 +0200

    V1tigres

--- romanizer.js
+++ romanizer.js
@@ -1,2 +1,28 @@
 function romanize (num) {
+    var res = "";
+    if(num<4)
+    {
+        res = imprimirCaracter(num, "I")
+    } 
+    else if (num<9) 
+    {
+        res = "V";
+        res += imprimirCaracter(num-5, "I");
+    }
+    if(num%5 == 4){
+        res = "IV";
+    }
+    if(num%5 == 1){
+        res = "IX";
+    }
+    return res;
+}
+
+function imprimirCaracter(numeroVeces, caracter) {
+    res = "";
+    for(i=numeroVeces;i>0;i--)
+                {
+                    res= res + caracter;
+                }
+    return res;
 }
